014a5e10a56fcbaee2f0ef74c55a3ca8a5f2879a
Benjamin Arbibe <benjamin.arbibe@sap.com>
2018-10-07 19:32:03 +0300

    v0.3

--- src/App.js
+++ src/App.js
@@ -6,20 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <Map google={this.props.google} zoom={14}>
-
-              <Marker onClick={this.onMarkerClick}
-                      name={'Current location'} />
-
-              <InfoWindow onClose={this.onInfoWindowClose}>
-
-              </InfoWindow>
-          </Map>
       </div>
     );
   }
 }
 
-export default GoogleApiWrapper({
-    apiKey: ("AIzaSyC4Kbb_4IwSPtb5LQi0d0oljGDZFXifjwk")
-})(App)
+export default App
